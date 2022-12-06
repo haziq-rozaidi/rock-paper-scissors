b897d6b68bf9bf313436fab92b6fd63c8e43b721
Haziq Rozaidi <haziq050946@gmail.com>
2022-12-06 12:15:47 +0800

    Add function to get computer's choice

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,8 @@ function getRandomInt(max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * max);
 }
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[getRandomInt(choices.length)];
+}
